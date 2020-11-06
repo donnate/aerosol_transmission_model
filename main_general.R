afd7a2f8a295fef3c4b4d6217bd788d3251ff3fb
WeiyiQian <412117899@qq.com>
2020-11-06 12:18:08 -0600

    Add files via upload

--- main_general.R
+++ main_general.R
@@ -5,20 +5,37 @@ compute_quanta_emission_rate <- function(cv, ci, inhalation, activity){
   ### This fucnction computes the quanta emission rate (per hour) given room parameters
   D <- c(0.8 * 10^(-6), 1.8 * 10^(-6), 3.5 * 10^(-6), 5.5 * 10^(-6))
   Vd <- 4/3 * pi * (D / 2)^3 
-  return(cv * ci * inhalation * )
-  
+  ### 'activity' is a vector of 4 droplets' distribution of 4 different size during a certain activity
+  return(cv * ci * inhalation * sum(activity * Vd))
 }
 
-compute_quanta_inhaled_per_person <- function(emission, removal, inhalation, volume){
+compute_quanta_inhaled_per_person <- function(emission, removal, infectious_people, inhalation, volume, duration){
   ### This fucnction computes the number of quanta inhaled by each participant 
   ### over the course of the event
-  return()
+  n <- function(t) {emission * infectious_people / (volume * removal) * (1 - exp(- removal * t))}   
+  ### 'inhalation' is a vector of all susceptible people's inhalation rate
+  return(integrate(n, 0, duration) * inhalation)
 }
 
-compute_distribution_infections <- function(){
+compute_distribution_infections <- function(inhaled, susceptible_people){
   #### This function coomputes the distribution (not just the average) of people that 
   #### will be infected over the course of the event
-  #### TO DO
+  ### 'inhaled' is a vector of all susceptible people's inhaled quanta
+  p <- 1 - exp(- inhaled)
+  r <- runif(1000 * susceptible_people)           ### random number pool should be larger than number of people
+  N = 500
+  c <- numeric(length(N))
+  for(i in 1:N){                                ## N random samples to show a probability distribution
+    d <- sample(r, susceptible_people)
+    for(j in length(susceptible_people)){
+      if(d[j] <= p[j]){
+        c[i] <- c[i] + 1
+      }
+    }
+  }
+  P <- as.data.frame(table(c))
+  P[,2] <- P[,2] / N
+  return(P)
 }
 
 compute_distribution_hospitalizations <- function(){
