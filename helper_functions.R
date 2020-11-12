a651934d40aab28a7a52350d841316c19a6966ae
WeiyiQian <412117899@qq.com>
2020-11-11 18:24:54 -0600

    Add files via upload

--- helper_functions.R
+++ helper_functions.R
@@ -8,12 +8,16 @@ extract_surface <- function(length, width){
   return(length * width)
 }
 
-extract_first_order <- function(ventilation, decay, deposition, control){
-  ### TO DO
-  return()
+extract_first_order <- function(Ventilation, control){
+  ventilation <- c(0.5, 3, 10)          # (natural, mechanical, mechanical)
+  decay <- 0.63
+  deposition <- 0.24
+  return((ventilation[Ventilation] + decay + deposition) * control)
 }
 
-extract_ventilation_rate_per_person <- function(){
-  ### TO DO
+extract_ventilation_rate_per_person <- function(activity, age){
+  # find inhalation rate using 'inhalation rate.csv'
+  
+
   return()
 }
