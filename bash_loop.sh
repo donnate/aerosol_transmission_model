804ed85525d3008a0993704405ebd84f6a7343f6
donnate <claire.donnat@gmail.com>
2020-12-11 16:47:48 -0800

    bash scripts

--- bash_loop.sh
+++ bash_loop.sh
@@ -16,5 +16,31 @@
 ml R
 for ind in {1..192}
 do
-   sbatch bash_script ${ind}
+   job_file="jobs/job${ind}.job"
+   echo ${job_file}
+   if [ -f ${job_file} ]
+   then 
+	  rm ${job_file}
+	  rm "out/${name}.err"
+	  rm "out/${name}.out"
+	fi
+	echo "#!/bin/bash
+#
+#SBATCH --job-name=compute_prevalence
+#SBATCH --output=/scratch/users/cdonnat/aerosol_transmission_model/out/prevalence.out
+#SBATCH --error=/scratch/users/cdonnat/aerosol_transmission_model/out/error.err
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --qos=normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cdonnat@stanford.edu
+#SBATCH --partition=hns,stat,normal,owners
+# load modules
+
+ml R
+Rscript $1 ${ind}
+">>${job_file}
+	sbatch ${job_file}
 done
